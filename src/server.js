b0febd6e6323c576e01b7beb0986fcfea02131bb
Minsoo, Kim <thouuy@gmail.com>
2022-07-15 15:34:40 +0900

    Added meta tag scrapper

--- src/server.js
+++ src/server.js
@@ -1,21 +1,35 @@
-const redisIp = '192.168.217.132';
+const redisIp = '127.0.0.1';
 const redisPort = '6379';
 const express = require('express');
 const { mainModule } = require('process');
+const fetch = require('node-fetch')
+const cheerio = require('cheerio');
 
 const app = express();
 const server = require('http').createServer(app);
+
+const ifaces = require('os').networkInterfaces();
+let address;
+Object.keys(ifaces).forEach(dev => {
+	ifaces[dev].filter(details => {
+		if (details.family === 'IPv4' && details.internal === false) {
+			address = details.address;
+		}
+	});
+});
+
 const io = require('socket.io')(server, {
 	'cors': {
-		"origin" : "http://localhost:3000",
-		"methods" : "GET,HEAD,PUT,PATCH,POST,DELETE",
-		"credentials" : true
+		'origin'  : "http://" + address + ":3000",
+		'methods' : "GET,HEAD,PUT,PATCH,POST,DELETE",
+		'credentials' : true
 	},
 	'allowEIO3': true // false by default
 });
 const redis = require('socket.io-redis');
 
 server.listen(3000, function() {
+	console.log(address);
 	console.log('listening on *:3000');
 });
 
@@ -27,7 +41,7 @@ app.get('/', function(req, res) {
 });
 
 // TODO : Adapting Redis
-//io.adapter(redis({ host: redisIp, port: redisPort }));
+io.adapter(redis({ host: redisIp, port: redisPort }));
 
 io.on('connection', function(socket) {
 	
@@ -90,9 +104,38 @@ io.on('connection', function(socket) {
 		const fromUser = io.of("/").sockets.get(from);
 		const toUser = to != null ? io.of("/").sockets.get(to) : null;
 		
+		var target = to != null ? to : roomId;
+		
 		// console.log("fromUser", fromUser);
 		// console.log("toUser", toUser);
 		
+
+		let regex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+		let metadata = {};
+		if (regex.test(message)) {
+			console.log(">>>>> URL string contained..")
+			url = message.match(regex)
+			console.log('url', url);
+			urlScrapper(url[0]).then((htmlStr) => {
+				//console.log(htmlStr);
+				metadata = scrapMetaTag(htmlStr);
+				console.log(metadata);
+
+				if (Object.keys(metadata).length > 0) {
+					var metadataResponse = {
+						title : metadata['og:title'],
+						url   : metadata['og:url'],
+						image : metadata['og:image'],
+						description : metadata['og:description'],
+					}
+					console.log('>>>>> [metadata_recv > emit()] target', target);
+					socket.to(target).emit('metadata_recv', metadataResponse);
+				}
+			});
+		}
+
+		
+
 		var response = {
 			'type' : to == null ? 'message' : 'whisper',
 			'from' : from,
@@ -104,8 +147,8 @@ io.on('connection', function(socket) {
 		};
 		console.log('>>>>> [chat] response : ', response);
 
-		var target = to != null ? to : roomId;
-		console.log('>>>>> [chat] target', target);
+		
+		console.log('>>>>> [chat_recv > emit()] target', target);
 		socket.to(target).emit('chat_recv', response);
 		// io.to(roomId).emit('chat_recv', response);
 	});
@@ -123,7 +166,8 @@ io.on('connection', function(socket) {
 			'buffer' : image.toString('base64'),
 			'time' : Date.now()
 		};
-		var target = to != null ? to : roomId;
+		//var target = to != null ? to : roomId;
+		var target = roomId;
 		console.log(">>>>>> [image] target : ", target);
 		socket.to(target).emit('image_recv', imageData);
 	});
@@ -176,3 +220,26 @@ io.on('connection', function(socket) {
 		io.emit('exitRoom', response);
 	});
 });
+
+async function urlScrapper(url) {
+	const response = await fetch(url);
+	const data = await response.text();
+	return data;
+}
+
+function scrapMetaTag(htmlStr) {
+	const $ = cheerio.load(htmlStr);
+	const meta = $('meta');
+	const metaData = {};
+	Object.keys(meta).forEach((key) => {
+		if(meta[key].attribs !== undefined){
+			if(meta[key].attribs.property && meta[key].attribs.content){
+				metaData[meta[key].attribs.property] = meta[key].attribs.content;
+			}
+			if(meta[key].attribs.name && meta[key].attribs.content){
+				metaData[meta[key].attribs.name] = meta[key].attribs.content;
+			}
+		}
+	});
+	return metaData;
+}
